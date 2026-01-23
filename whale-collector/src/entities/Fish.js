0fc92b605a4f0ecde06bcbefbecd9632d04e7025
GWS-game <glidingwhaleshark@gmail.com>
2026-01-23 10:57:52 +0900

    Update Fish.js

--- whale-collector/src/entities/Fish.js
+++ whale-collector/src/entities/Fish.js
@@ -1,1 +1,14 @@
+import { Friend } from "./Friend.js";
+
+export class Fish extends Friend {
+  constructor(scene, shark) {
+    super(scene, shark, "fish_small");
+
+    /* ===============================
+     * 크기 & 깊이
+     * =============================== */
+    this.sprite.setScale(0.5);
+    this.sprite.setDepth(3);
+  }
+}
 

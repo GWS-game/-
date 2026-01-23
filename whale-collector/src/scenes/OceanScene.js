89dea86e602e28808cfdac7cbed75bf4e6995884
GWS-game <glidingwhaleshark@gmail.com>
2026-01-23 10:58:52 +0900

    Update OceanScene.js

--- whale-collector/src/scenes/OceanScene.js
+++ whale-collector/src/scenes/OceanScene.js
@@ -1,6 +1,8 @@
-import { Config } from "../core/Config.js";
 import { Shark } from "../entities/Shark.js";
+import { Fish } from "../entities/Fish.js";
+
 import { MovementSystem } from "../systems/MovementSystem.js";
+import { ParticleSystem } from "../systems/ParticleSystem.js";
 
 export class OceanScene extends Phaser.Scene {
   constructor() {
@@ -11,7 +13,7 @@ export class OceanScene extends Phaser.Scene {
     const { width, height } = this.scale;
 
     /* ===============================
-     * 배경
+     * 배경 (낮 바다)
      * =============================== */
     this.background = this.add.image(
       width / 2,
@@ -22,36 +24,46 @@ export class OceanScene extends Phaser.Scene {
     this.background.setDepth(-10);
 
     /* ===============================
-     * 고래상어 생성
+     * 플레이어 (고래상어)
      * =============================== */
     this.shark = new Shark(this);
 
     /* ===============================
-     * 앰비언트 사운드
+     * 첫 번째 친구 (물고기)
      * =============================== */
-    this.ambientSound = this.sound.add(
-      "ambient_deep_sea",
-      { loop: true, volume: 0.4 }
-    );
-    this.ambientSound.play();
+    this.fish = new Fish(this, this.shark);
 
     /* ===============================
-     * 리사이즈 대응 (PC 필수)
+     * 리사이즈 대응 (PC 웹 필수)
      * =============================== */
     this.scale.on("resize", this.onResize, this);
   }
 
   update(time, delta) {
     /* ===============================
-     * 이동 시스템
+     * 고래상어 이동
      * =============================== */
     MovementSystem.update(this.shark, delta);
+
+    /* ===============================
+     * 헤엄 잔상 파티클
+     * =============================== */
+    ParticleSystem.update(this, this.shark, delta);
+
+    /* ===============================
+     * 친구 업데이트
+     * =============================== */
+    if (this.fish) {
+      this.fish.update(delta);
+    }
   }
 
   onResize(gameSize) {
     const { width, height } = gameSize;
-    this.background.setPosition(width / 2, height / 2);
-    this.background.setDisplaySize(width, height);
+
+    if (this.background) {
+      this.background.setPosition(width / 2, height / 2);
+      this.background.setDisplaySize(width, height);
+    }
   }
 }
-
